2c60d5e8b2ae27f0c95c133da597f8c549cf017d
Dwayne Munro <dwaynemunro1845@gmail.com>
2018-08-19 21:57:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ function happyHolidaysTo(name) {
   return `Happy holidays, ${name}!`;
 }
 
-function HappyHolidayTo(holiday, name) {
-  return `Happy ${holiday}, ${name}!`
+function happyHolidayTo(holiday, name) {
+  return `Happy ${holiday}, ${name}!`;
 }
 
 function holidayCountdown(days, holiday) {
